59aa83413f8fa9c220b80fc35ef8b61af12fc97c
Josh Perez <josh@goatslacker.com>
2015-10-02 15:28:57 -0700

    Removes this.dispatch

--- src/utils/functions.js
+++ src/utils/functions.js
@@ -1,9 +1,5 @@
 export const isFunction = x => typeof x === 'function'
 
-export function isPromise(obj) {
-  return !!obj && (typeof obj === 'object' || typeof obj === 'function') && typeof obj.then === 'function'
-}
-
 export function eachObject(f, o) {
   o.forEach((from) => {
     Object.keys(Object(from)).forEach((key) => {
